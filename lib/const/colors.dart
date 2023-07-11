0259dc5b1e691674539fe52971d3e6cf2ee0f089
wahidmurad1 <genie.murad@gmail.com>
2023-07-11 13:46:20 +0600

    after homescaffold color in color file and after all Refactor

--- lib/const/colors.dart
+++ lib/const/colors.dart
@@ -3,3 +3,4 @@ import 'package:flutter/material.dart';
 const greenColor = Colors.green;
 const redColor = Colors.red;
 const scaffoldColor = Colors.black;
+const homeScaffoldColor = Color(0XFF272727);
